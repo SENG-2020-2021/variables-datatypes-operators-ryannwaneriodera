3243004889b10c4be32e03a9dceb63ac006389cf
ryannwaneriodera <71433796+ryannwaneriodera@users.noreply.github.com>
2020-09-24 08:09:07 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
    int defaultedDays=14,amountPerDay=500,amountToPay;
-   int daysPaidFor,amountLeftToPay,daysNotPaid,amountPayed=3200,amountNotInDay;
+   int daysPaidFor,amountLeftToPay,daysNotPaid,amountPayed=3500,amountNotInDay;
    
    amountToPay=defaultedDays*amountPerDay;
    System.out.println("the amount to pay is:"+amountToPay);
@@ -12,9 +12,16 @@ class Main {
    amountNotInDay=amountPayed%amountPerDay;
    System.out.println("the amount that doesnt cover a day is:"+amountNotInDay);
    
+   if(amountNotInDay>0){
+   
    amountLeftToPay=((defaultedDays-daysPaidFor)*amountPerDay)+(amountPerDay-amountNotInDay);
    System.out.println("the amount left to pay is:"+amountLeftToPay);
+   }
    
+   else{
+	   amountLeftToPay=((defaultedDays-daysPaidFor)*amountPerDay);
+   System.out.println("the amount left to pay is:"+amountLeftToPay);
+   }
    daysNotPaid=defaultedDays-daysPaidFor;
    System.out.println("the days not paid for are:"+daysNotPaid);
 
